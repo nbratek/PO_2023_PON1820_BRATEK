84b19547065e9eac97cbd4bebd3469dc70941393
nbratek <bratek@student.agh.edu.pl>
2023-10-14 11:29:47 +0200

    lab1 - updating MoveDirection class

--- oolab/src/main/java/agh/ics/oop/model/MoveDirection.java
+++ oolab/src/main/java/agh/ics/oop/model/MoveDirection.java
@@ -1,19 +1,5 @@
 package agh.ics.oop.model;
 
 public enum MoveDirection {
-    FORWARD("Zwierzak idzie do przodu"),
-    BACKWARD("Zwierzak idzie do tyłu"),
-    RIGHT("Zwierzak skręca w prawo"),
-    LEFT("Zwierzak skręca w lewo");
-
-    private final String description;
-
-    MoveDirection(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return description;
-    }
+    FORWARD, BACKWARD, RIGHT, LEFT
 }
