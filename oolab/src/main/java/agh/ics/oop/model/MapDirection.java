62dabfb1ba592718d3592d1b81d7bd4009a08b08
nbratek <bratek@student.agh.edu.pl>
2023-10-19 16:05:46 +0200

    lab2 - creating MapDirection class

---
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -0,0 +1,43 @@
+package agh.ics.oop.model;
+
+public enum MapDirection {
+    NORTH, SOUTH, WEST, EAST;
+
+    @Override
+    public String toString() {
+        return switch (this) {
+            case NORTH -> "Północ";
+            case SOUTH -> "Południe";
+            case EAST -> "Wschód";
+            case WEST -> "Zachód";
+        };
+    }
+
+    public MapDirection next() {
+        return switch (this) {
+            case NORTH -> EAST;
+            case SOUTH -> WEST;
+            case EAST -> SOUTH;
+            case WEST -> NORTH;
+        };
+    }
+
+    public MapDirection previous() {
+        return switch (this) {
+            case NORTH -> WEST;
+            case SOUTH -> EAST;
+            case EAST -> NORTH;
+            case WEST -> SOUTH;
+        };
+    }
+
+    public Vector2d toUnitVector() {
+        return switch (this) {
+            case NORTH -> new Vector2d(0, 1);
+            case SOUTH -> new Vector2d(0, -1);
+            case WEST -> new Vector2d(-1, 0);
+            case EAST -> new Vector2d(1, 0);
+        };
+    }
+
+}
