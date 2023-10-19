ec61c51d1425dc96ca24697b2b6e836e072a7cf8
nbratek <bratek@student.agh.edu.pl>
2023-10-19 15:18:56 +0200

    lab2 - verifying Vector2d class

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,6 +1,7 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
 
 public class World {
 
@@ -8,9 +9,20 @@ public class World {
     public static final String STOP = "system zakończył działanie";
 
     public static void main(String[] args) {
+
+        //lab1
+
         System.out.println(START);
         run(OptionsParser.parse(args));
         System.out.println(STOP);
+
+        // lab2
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
     }
 
     public static void run(MoveDirection[] moveDirections) {
