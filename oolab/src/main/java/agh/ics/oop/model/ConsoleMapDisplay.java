8bb08e83e76f26381afe749c6ed6a44f69bc2af9
nbratek <bratek@student.agh.edu.pl>
2023-11-16 20:52:49 +0100

    lab6 - creating ConsoleMapDisplay and MapChangeListener

---
+++ oolab/src/main/java/agh/ics/oop/model/ConsoleMapDisplay.java
@@ -0,0 +1,14 @@
+package agh.ics.oop.model;
+
+public class ConsoleMapDisplay implements MapChangeListener {
+
+    private int counter = 0;
+
+    @Override
+    public void mapChanged(WorldMap worldMap, String message) {
+        counter++;
+        System.out.println(message);
+        System.out.println(worldMap.toString());
+        System.out.println("total update: " + counter);
+    }
+}
