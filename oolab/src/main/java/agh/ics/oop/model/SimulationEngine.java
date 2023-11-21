b4bcef81ea7b76a14659286d35da28a7a017c78b
nbratek <bratek@student.agh.edu.pl>
2023-11-21 21:13:48 +0100

    lab7 - creating SimulationEngine

---
+++ oolab/src/main/java/agh/ics/oop/model/SimulationEngine.java
@@ -0,0 +1,59 @@
+package agh.ics.oop.model;
+
+import agh.ics.oop.Simulation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SimulationEngine {
+
+    private List<Simulation> simulationList;
+
+    public SimulationEngine(List<Simulation> simulationList) {
+        this.simulationList = simulationList;
+    }
+
+    public void runSync(){
+        for (Simulation simulation: simulationList){
+            simulation.run();
+        }
+    }
+
+    public void runAsync(){
+        List<Thread> threads = new ArrayList<>();
+        for (Simulation simulation: simulationList){
+            Thread thread = new Thread(simulation::run);
+            threads.add(thread);
+            thread.start();
+        }
+        awaitSimulationsEnd(threads);
+    }
+
+    public void awaitSimulationsEnd(List<Thread> threads){
+        for (Thread thread: threads){
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void runAsyncInThreadPool() {
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+
+        for (Simulation simulation : simulationList) {
+            executorService.submit(simulation::run);
+        }
+
+        executorService.shutdown();
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
