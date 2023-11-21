eb1abaf5f2bf0f82e8f3015058c17e8034cefd69
nbratek <bratek@student.agh.edu.pl>
2023-11-21 21:14:30 +0100

    lab7 - updating AbstractWorldMap, RectangularMap

--- oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
+++ oolab/src/main/java/agh/ics/oop/model/RectangularMap.java
@@ -1,19 +1,19 @@
 package agh.ics.oop.model;
 
-import agh.ics.oop.model.util.MapVisualizer;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.UUID;
 
 public class RectangularMap extends AbstractWorldMap {
 
     private final int width;
     private final int height;
 
+
+
     public RectangularMap(int width, int height) {
         this.width = width;
         this.height = height;
     }
+
 
     @Override
     public boolean canMoveTo(Vector2d position) {
