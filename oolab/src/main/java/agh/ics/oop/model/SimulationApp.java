7a405b07c70ed2eaf3f15bb4f5ca8898280c71e6
nbratek <bratek@student.agh.edu.pl>
2023-11-28 21:33:17 +0100

    lab8 - updating simulation.fxml, SimulationApp and SimulationPresenter

--- oolab/src/main/java/agh/ics/oop/model/SimulationApp.java
+++ oolab/src/main/java/agh/ics/oop/model/SimulationApp.java
@@ -21,16 +21,9 @@ public class SimulationApp extends Application {
         BorderPane viewRoot = loader.load();
         SimulationPresenter presenter = loader.getController();
         configureStage(primaryStage, viewRoot);
-
-        List<String> args = getParameters().getRaw();
-        List<MoveDirection> directions = OptionsParser.parse(args.toArray(new String[0]));
-        List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
         RectangularMap rectangularMap = new RectangularMap(4,4);
-        presenter.setWorldMap(rectangularMap);
         rectangularMap.addObserver(presenter);
-        Simulation simulation = new Simulation(rectangularMap, positions, directions);
-        simulation.run();
-
+        presenter.setWorldMap(rectangularMap);
     }
 
     private void configureStage(Stage primaryStage, BorderPane viewRoot) {
--- oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
+++ oolab/src/main/java/agh/ics/oop/presenter/SimulationPresenter.java
@@ -1,11 +1,18 @@
 package agh.ics.oop.presenter;
 
-import agh.ics.oop.model.MapChangeListener;
-import agh.ics.oop.model.WorldMap;
+import agh.ics.oop.OptionsParser;
+import agh.ics.oop.Simulation;
+import agh.ics.oop.model.*;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.List;
 
 public class SimulationPresenter implements MapChangeListener {
+
+    @FXML
+    private TextField movesTextField;
 
     @FXML
     private Label infoLabel;
@@ -24,5 +31,14 @@ public class SimulationPresenter implements MapChangeListener {
     @Override
     public void mapChanged(WorldMap worldMap, String message) {
         drawMap(message);
+    }
+
+    @FXML
+    public void onSimulationStartClicked() {
+        String[] moveArray = movesTextField.getText().split(",");
+        List<MoveDirection> directions = OptionsParser.parse(moveArray);
+        List<Vector2d> positions = List.of(new Vector2d(2, 2), new Vector2d(3, 4));
+        Simulation simulation = new Simulation(worldMap, positions, directions);
+        simulation.run();
     }
 }
